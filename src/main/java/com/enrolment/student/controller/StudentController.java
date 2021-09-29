9d8b294b52c2a8c51500db2d2f382a8a11bf8979
Subbareddy Nallamachu <subbareddynallamachu@gmail.com>
2021-09-30 02:24:53 +0530

    ResponseEntity Changes

--- src/main/java/com/enrolment/student/controller/StudentController.java
+++ src/main/java/com/enrolment/student/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.enrolment.student.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +21,7 @@ import com.enrolment.student.service.StudentService;
 import com.enrolment.student.util.ObjectConversion;
 
 @RestController
-@RequestMapping(path = "/v1/api")
+@RequestMapping(path = "/v1/api/student")
 public class StudentController {
 
 	@Autowired
@@ -29,37 +31,41 @@ public class StudentController {
 	public ObjectConversion conversion;
 
 	@GetMapping(path = "/all-students", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<StudentDTO> getAllStudents() {
+	public ResponseEntity<List<StudentDTO>> getAllStudents() {
 		List<Student> students = studentService.getAllStudents();
-		return conversion.studentEntityToDTOList(students);
+		return (students !=null)?new ResponseEntity<List<StudentDTO>>(conversion.studentEntityToDTOList(students),HttpStatus.OK)
+				:new ResponseEntity("Exception while getting all students",HttpStatus.EXPECTATION_FAILED);
 	}
 
-	@GetMapping(path = "/student/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public StudentDTO getStudentById(@PathVariable(name = "id") Long id) {
+	@GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<StudentDTO> getStudentById(@PathVariable(name = "id") Long id) {
 		if (id == null)
 			return null;
 		Student student = studentService.getStudentById(id);
-		return (student != null) ? conversion.studentEntityToDTO(student) : null;
+		return (student !=null)?new ResponseEntity<StudentDTO>(conversion.studentEntityToDTO(student),HttpStatus.OK)
+				:new ResponseEntity("Exception while getting student by id",HttpStatus.EXPECTATION_FAILED);
 	}
 
 	@PostMapping(path = "/new-student", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public StudentDTO addNewStudent(@RequestBody StudentDTO studentDTO) {
+	public ResponseEntity<StudentDTO> addNewStudent(@RequestBody StudentDTO studentDTO) {
 		if (studentDTO == null) {
 			return null;
 		}
 		Student student = conversion.studentDTOToEntity(studentDTO);
 		student = studentService.addNewStudent(student);
-		return conversion.studentEntityToDTO(student);
+		return (student !=null)?new ResponseEntity<StudentDTO>(conversion.studentEntityToDTO(student),HttpStatus.OK)
+				:new ResponseEntity("Exception while adding new student",HttpStatus.EXPECTATION_FAILED);
 	}
 
 	@PutMapping(path = "/update-student", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public StudentDTO updateStudent(@RequestBody StudentDTO studentDTO) {
+	public ResponseEntity<StudentDTO> updateStudent(@RequestBody StudentDTO studentDTO) {
 		if (studentDTO == null)
 			return null;
 
 		Student student = conversion.studentDTOToEntity(studentDTO);
 		student = studentService.updateStudent(student);
-		return (student != null) ? conversion.studentEntityToDTO(student) : null;
+		return (student !=null)?new ResponseEntity<StudentDTO>(conversion.studentEntityToDTO(student),HttpStatus.OK)
+				:new ResponseEntity("Exception while updating student",HttpStatus.EXPECTATION_FAILED);
 	}
 
 	@DeleteMapping(path = "/delete-student/{id}")
@@ -68,15 +74,17 @@ public class StudentController {
 	}
 
 	@GetMapping(path = "/course-without-student", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<StudentDTO> getStudentsWithoutAnyStudents() {
+	public ResponseEntity<List<StudentDTO>> getStudentsWithoutAnyStudents() {
 		List<Student> students = studentService.getStudentsWithoutAnyCourses();
-		return (students != null) ? conversion.studentEntityToDTOList(students) : null;
+		return (students !=null)?new ResponseEntity<List<StudentDTO>>(conversion.studentEntityToDTOList(students),HttpStatus.OK)
+				:new ResponseEntity("Exception while getting all students",HttpStatus.EXPECTATION_FAILED);
 	}
 
 	@GetMapping(path = "/student-by-course/{courseName}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<StudentDTO> getStudentsByCourse(@PathVariable(name = "courseName") String courseName) {
+	public ResponseEntity<List<StudentDTO>> getStudentsByCourse(@PathVariable(name = "courseName") String courseName) {
 		List<Student> students = studentService.getStudentByCourse(courseName);
-		return (students != null) ? conversion.studentEntityToDTOList(students) : null;
+		return (students !=null)?new ResponseEntity<List<StudentDTO>>(conversion.studentEntityToDTOList(students),HttpStatus.OK)
+				:new ResponseEntity("Exception while getting all students",HttpStatus.EXPECTATION_FAILED);
 	}
 
 }
